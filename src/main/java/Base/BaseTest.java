b9752882dbd08d20f710beda283345e74f144229
AshviniSusar <ashvinisusar2001@gmail.com>
2024-03-27 09:37:31 +0530

    my commit

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -29,84 +29,76 @@ import com.beust.jcommander.SubParameter;
 import Utils.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class BaseTest {
 	public static WebDriver driver;
 	public ExtentSparkReporter sparkReporter;
 	public ExtentReports extent;
 	public ExtentTest logger;
-	
-	
-	
-	
-	
 
 	@BeforeTest
-	public void beforeTestMethod()
-	{
-		sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir")+File.separator+"reports"+File.separator+"SDETAADDE");
+	public void beforeTestMethod() {
+		sparkReporter = new ExtentSparkReporter(
+				System.getProperty("user.dir") + File.separator + "reports" + File.separator + "SDETAADDE");
 		extent = new ExtentReports();
 		extent.attachReporter(sparkReporter);
 		sparkReporter.config().setTheme(Theme.DARK);
-		extent.setSystemInfo("HostName","ashvini");
-		extent.setSystemInfo("Username","ashu");
+		extent.setSystemInfo("HostName", "ashvini");
+		extent.setSystemInfo("Username", "ashu");
 		sparkReporter.config().setDocumentTitle("AutomationReport");
 		sparkReporter.config().setReportName("Automation Tests Result by Ashu");
-		
+
 	}
-	
 
 	@BeforeMethod
 	@Parameters("browser")
-	public void beforeMethod(String browser,Method testMethod)
-	{
-	logger = extent.createTest(testMethod.getName());
+	public void beforeMethod(String browser, Method testMethod) {
+		logger = extent.createTest(testMethod.getName());
 		setupDriver(browser);
 		driver.manage().window().maximize();
 		driver.get(Constants.url);
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	}
-	
+
 	@AfterMethod
 	public void afterMethod() {
 		ITestResult result = null;
-		if (result.getStatus()==ITestResult.FAILURE) {
-			logger.log(Status.FAIL,MarkupHelper.createLabel(result.getName()+" - Test Case Failed",ExtentColor.RED));
-			logger.log(Status.FAIL,MarkupHelper.createLabel(result.getThrowable()+" - Test Case Failed",ExtentColor.RED));}
-		
-			else if (result.getStatus()==ITestResult.SKIP) {
-				logger.log(Status.SKIP,MarkupHelper.createLabel(result.getName()+" - Test Case Skipped",ExtentColor.ORANGE));}
-			else if (result.getStatus()==ITestResult.SUCCESS) {
-				
-				logger.log(Status.PASS,MarkupHelper.createLabel(result.getName()+" - Test Case PASS",ExtentColor.GREEN));}
-               driver.quit();
-			}
-				
-			
-		
-		
-	
+		if (result.getStatus() == ITestResult.FAILURE) {
+			logger.log(Status.FAIL,
+					MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
+			logger.log(Status.FAIL,
+					MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
+		}
+
+		else if (result.getStatus() == ITestResult.SKIP) {
+			logger.log(Status.SKIP,
+					MarkupHelper.createLabel(result.getName() + " - Test Case Skipped", ExtentColor.ORANGE));
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+
+			logger.log(Status.PASS,
+					MarkupHelper.createLabel(result.getName() + " - Test Case PASS", ExtentColor.GREEN));
+		}
+		driver.quit();
+	}
+
 	@AfterTest
 	public void afterTest() {
 		extent.flush();
 	}
-	
 
+	public void setupDriver(String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
 
-public void  setupDriver(String browser) {
-	if (browser.equalsIgnoreCase("chrome")) {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();}
-	
 		else if (browser.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-	     driver = new FirefoxDriver();}
-	
+			driver = new FirefoxDriver();
+		}
+
 		else if (browser.equalsIgnoreCase("edge")) {
 			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();}
+			driver = new EdgeDriver();
+		}
+	}
 }
-}
-		
-
-
